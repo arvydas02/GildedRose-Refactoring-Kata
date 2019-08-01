b288ef2ad370c5d4426dcd7d15c4fd044e794436
Arvydas <arvydas.geniussports@gmail.com>
2019-08-01 15:13:15 +0300

    test: adding test for specified item - Backstage passes

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -121,6 +121,43 @@ class GildedRoseTest extends TestCase {
                         'quality' => 80,
                     ]
                 ],
+            // Specified Item Tests - Backstage passes to a TAFKAL80ETC concert
+            'Item Backstage passes Sell In Value Reduce and Quality Value Increase' =>
+                [
+                    new Item('Backstage passes to a TAFKAL80ETC concert', 20, 0),
+                    [
+                        'name' => 'Backstage passes to a TAFKAL80ETC concert',
+                        'sellIn' => 19,
+                        'quality' => 1,
+                    ]
+                ],
+            'Item Backstage passes Sell In Value Reduce and Quality Value Increase by 2 With Sell In days lower than 10 days' =>
+                [
+                    new Item('Backstage passes to a TAFKAL80ETC concert', 9, 0),
+                    [
+                        'name' => 'Backstage passes to a TAFKAL80ETC concert',
+                        'sellIn' => 8,
+                        'quality' => 2,
+                    ]
+                ],
+            'Item Backstage passes Sell In Value Reduce and Quality Value Increase by 3 With Sell In days Lower than 5' =>
+                [
+                    new Item('Backstage passes to a TAFKAL80ETC concert', 2, 0),
+                    [
+                        'name' => 'Backstage passes to a TAFKAL80ETC concert',
+                        'sellIn' => 1,
+                        'quality' => 3,
+                    ]
+                ],
+            'Item Backstage passes Sell In Value Reduce and Quality Value Equal to zero With Sell In days Equal to zero' =>
+                [
+                    new Item('Backstage passes to a TAFKAL80ETC concert', 0, 50),
+                    [
+                        'name' => 'Backstage passes to a TAFKAL80ETC concert',
+                        'sellIn' => -1,
+                        'quality' => 0,
+                    ]
+                ],
         ];
     }
 
