1057c5964826fe8e6ade15d40ac9eb75343af9fa
Arvydas <arvydas.geniussports@gmail.com>
2019-08-01 19:26:33 +0300

    refactor: create backstage pass product class

---
+++ src/Products/BackstagePassesProduct.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Products;
+
+/**
+ * Class BackstagePassesProduct
+ * @package App\Products
+ */
+class BackstagePassesProduct extends AbstractProduct
+{
+    /**
+     *  Update Item quality and sell in values
+     */
+    public function update(): void
+    {
+        // Update quality
+        if ($this->item->quality > 10) {
+            $this->item->quality++;
+        }
+
+        if ($this->item->quality <= 10 && $this->item->quality > 5) {
+            $this->item->quality += 2;
+        }
+
+        if ($this->item->quality >= 5 && $this->item->quality > 0) {
+            $this->item->quality += 3;
+        }
+
+        $this->checkAndUpdateQualityByRange();
+
+        // Update Sell in
+        $this->updateSellIn();
+
+        if ($this->item->sell_in < 0) {
+            $this->item->quality = 0;
+        }
+    }
+}
