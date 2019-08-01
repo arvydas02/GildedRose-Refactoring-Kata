c3e2b8ae423f60e6d5bc4ee868fcea989abf7989
Arvydas <arvydas.geniussports@gmail.com>
2019-08-01 21:40:12 +0300

    test: adding test for Conjured product

--- test/ConjuredProductTest.php
+++ test/ConjuredProductTest.php
@@ -3,77 +3,78 @@
 
 namespace App;
 
-use App\Products\CommonProduct;
+
+use App\Products\ConjuredProduct;
 use PHPUnit\Framework\TestCase;
 
-class CommonProductTest extends TestCase
+class ConjuredProductTest extends TestCase
 {
     public function itemProvider()
     {
         return [
             'Item Sell In and Quality Values Reduced With Sell in day more than One' =>
                 [
-                    new Item('+5 Dexterity Vest', 10, 20),
+                    new Item('Conjured Mana Cake', 10, 20),
                     [
-                        'name' => '+5 Dexterity Vest',
+                        'name' => 'Conjured Mana Cake',
                         'sellIn' => 9,
-                        'quality' => 19,
+                        'quality' => 18,
                     ]
                 ],
             'Item Sell In and Quality Values Reduced With Sell In Days Equal to zero' =>
                 [
-                    new Item('+5 Dexterity Vest', 0, 20),
+                    new Item('Conjured Mana Cake', 0, 20),
                     [
-                        'name' => '+5 Dexterity Vest',
+                        'name' => 'Conjured Mana Cake',
                         'sellIn' => -1,
-                        'quality' => 18,
+                        'quality' => 16,
                     ]
                 ],
             'Item Sell In and Quality Values Reduced With Sell In Days Lower than zero' =>
                 [
-                    new Item('+5 Dexterity Vest', -1, 20),
+                    new Item('Conjured Mana Cake', -1, 20),
                     [
-                        'name' => '+5 Dexterity Vest',
+                        'name' => 'Conjured Mana Cake',
                         'sellIn' => -2,
-                        'quality' => 18,
+                        'quality' => 16,
                     ]
                 ],
             'Item Sell In and Quality Values Reduced With Quality Max Value' =>
                 [
-                    new Item('Elixir of the Mongoose', 10, 50),
+                    new Item('Conjured Mana Cake', 10, 50),
                     [
-                        'name' => 'Elixir of the Mongoose',
+                        'name' => 'Conjured Mana Cake',
                         'sellIn' => 9,
-                        'quality' => 49,
+                        'quality' => 48,
                     ]
                 ],
             'Item Sell In and Quality Values Reduced With Quality Over Max Value' =>
                 [
-                    new Item('Elixir of the Mongoose', 10, 100),
+                    new Item('Conjured Mana Cake', 10, 100),
                     [
-                        'name' => 'Elixir of the Mongoose',
+                        'name' => 'Conjured Mana Cake',
                         'sellIn' => 9,
                         'quality' => 50,
                     ]
                 ],
             'Item Sell In and Quality Values Reduced With Quality lower than zero' =>
                 [
-                    new Item('Elixir of the Mongoose', 10, -1),
+                    new Item('Conjured Mana Cake', 10, -5),
                     [
-                        'name' => 'Elixir of the Mongoose',
+                        'name' => 'Conjured Mana Cake',
                         'sellIn' => 9,
                         'quality' => 0,
                     ]
                 ],
-            ];
+        ];
     }
 
     /**
      * @dataProvider itemProvider
      */
-    public function testCommonProduct($item, $results)
+    public function testConjuredProduct($item, $results)
     {
-        $product = new CommonProduct($item);
+        $product = new ConjuredProduct($item);
         $product->update();
 
         $this->assertEquals($item->name, $results['name']);
